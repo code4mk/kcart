1901a07ae0d40f5c6752cca948500e01e330e4e5
mostafa <radionishi@gmail.com>
2019-05-08 21:53:23 +0600

    kcart database

---
+++ database/2019_05_05_161124_create_kcarts_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKcartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kcarts', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->integer('auth_user')->nullable();
+            $table->string('coupon')->nullable();
+            $table->float('price',18,2)->nullable();
+            $table->float('discount',18,2)->nullable();
+            $table->float('cprice',18,2)->nullable();
+            $table->float('tax',18,2)->nullable();
+            $table->string('shipping_area')->nullable();
+            $table->float('total',18,2)->nullable();
+            $table->boolean('paid')->default(false);
+            $table->string('affiliate')->nullable();
+            $table->timestamps();
+        });
+        \DB::statement('ALTER TABLE kcarts AUTO_INCREMENT = 10000000');
+
+        Schema::create('kcart_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('kcart_id')->nullable();
+            $table->integer('product_id')->nullable();
+            $table->string('title')->nullable();
+            $table->string('slug')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->integer('order_limit')->nullable();
+            $table->string('size')->nullable();
+            $table->string('color')->nullable();
+            $table->string('image')->nullable();
+            $table->float('single_price',16,2)->nullable();
+            $table->float('price',16,2)->nullable();
+            $table->string('coupon')->nullable();
+            $table->string('coupon_type')->nullable();
+            $table->float('coupon_amount',16,2)->nullable();
+            $table->float('discount',16,2)->nullable();
+            $table->float('final_price',16,2)->nullable();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kcarts');
+        Schema::dropIfExists('kcart_items');
+    }
+}
